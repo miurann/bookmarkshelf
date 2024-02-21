23bfef19aebbdfb5e024da565def632937d3cac4
miurann <mn.camissha@gmail.com>
2024-02-22 07:50:52 +0900

    fix:seedとfly.tomlの修正

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+200.times do |index|
+  Bookmark.create!(
+      user: User.first,
+      title: Faker::JapaneseMedia::StudioGhibli.movie,
+      url: "http://xx#{index}.com",
+      memo: Faker::JapaneseMedia::StudioGhibli::character
+  )
+end
