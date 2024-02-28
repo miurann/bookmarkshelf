d5c4c933b75e08c6054adab4ec1e46ee1a7145ea
miurann <mn.camissha@gmail.com>
2024-02-29 02:31:11 +0900

    add:パンくず追加 #46

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,13 @@
+  crumb :bookmarks do
+    link "ブックマーク一覧", bookmarks_path
+  end
+
+  crumb :bookmark do |bookmark|
+    link "ブックマーク詳細", bookmark_path(bookmark)
+    parent :bookmarks
+  end
+
+  crumb :edit_bookmark do |bookmark|
+    link "ブックマーク編集"
+    parent :bookmark, bookmark
+  end
