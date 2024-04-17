4aeed8b90698f01ccac30ba1cca8bc7a38a9bf8a
miurann <mn.camissha@gmail.com>
2024-04-18 01:22:16 +0900

    add: ブックマーク登録、編集フォームにタグを追加 #78

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
+  has_many :bookmark_tags, dependent: :destroy
+  has_many :tags, through: :bookmark_tags
 
   validates :url, presence: true, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :title, length: { maximum: 255 }
