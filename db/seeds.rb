c3ee931a2f341b6da23b002b0742a53a9c022fcc
miurann <mn.camissha@gmail.com>
2024-11-05 01:36:33 +0900

    add:デモデータの作成、update後の遷移先変更、タグの表示を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,9 @@
-CoverImage.create!(
-  [
-    {
-      image: File.open('./app/assets/images/book28.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book29.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book30.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book31.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book32.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book33.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book34.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book35.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book36.png'),
-      standard: true
-    },
-    {
-      image: File.open('./app/assets/images/book19front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book20front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book21front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book22front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book23front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book24front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book25front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book26front.png'),
-      standard: false
-    },
-    {
-      image: File.open('./app/assets/images/book27front.png'),
-      standard: false
-    },
-  ]
-)
+tags = Tag.all
+bookmarks = Bookmark.all
+
+bookmarks.each do |bookmark|
+    BookmarkTag.create!(
+      bookmark_id: bookmark.id,
+      tag_id: tags.sample.id
+    )
+  end
